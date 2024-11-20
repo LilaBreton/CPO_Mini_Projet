aa326930a496f4753bd0b9f7c0da61d8881388e1
Lila Breton <lila.breton@epfedu.fr>
2024-11-20 10:23:37 +0100

    Création d'un nouveau projet versionné

---
+++ CPO_Lights_off_Breton_LePage/src/cpo_lights_off_breton_lepage/CPO_Lights_off_Breton_LePage.java
@@ -0,0 +1,18 @@
+/*
+ */
+package cpo_lights_off_breton_lepage;
+
+/**
+ *
+ * @author lilab
+ */
+public class CPO_Lights_off_Breton_LePage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
